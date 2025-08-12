56315498e963b31299e833c862c9678a683866d8
Dvonne-D-Thales <05566@smkrus.sch.id>
2025-08-12 11:24:33 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:latihan_11pplg1/login_page.dart';
+import 'package:latihan_11pplg1/pages/calculator_pages.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:LoginPage()
+      home:CalculatorPages()
     );
   }
 }
